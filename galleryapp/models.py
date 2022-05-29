d2774f1ee48f046d241b0863e0ef9427e59bbfb5
brie <briememo95@gmail.com>
2022-05-30 00:36:17 +0300

    create location class

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    @classmethod
+    def get_locations(cls):
+        return Location.objects.all()
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(name=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
