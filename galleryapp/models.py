7668ede60bd08bb1ef0bab6aa21c57c816de91ac
brie <briememo95@gmail.com>
2022-05-30 00:38:44 +0300

    add image class

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -20,3 +20,40 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    name = models.CharField(max_length=65)
+    description = models.TextField()
+    author = models.CharField(max_length=43, default='admin')
+    image = models.ImageField(upload_to='images/')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['date']
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        return cls.objects.filter(id=id).all()
+
+    @classmethod
+    def filter_by_location(cls, location):
+        return Image.objects.filter(location__name=location).all()
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def search_by_category(cls, category):
+        return cls.objects.filter(category__name__icontains=category)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
